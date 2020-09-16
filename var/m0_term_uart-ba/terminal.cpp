14c7fae6b0333a0610006fceeea7ef28047db1c0
wa1tnr <wa1tnr@users.noreply.github.com>
2020-09-16 02:30:17 +0000

    Fairly satisfying terminal 16 SEP 2020
    Tested with STM32F407 Discovery eForth v7.20
    Blue sloped cabinet
    
    Seems responsive.
    
    	modified:   terminal.cpp
    
    On branch lumex-bb-

--- var/m0_term_uart-ba/terminal.cpp
+++ var/m0_term_uart-ba/terminal.cpp
@@ -1,48 +1,17 @@
-// Thu May 21 22:31:03 UTC 2020 // STM32F407 Discovery running C.H. Ting's eForth
+// Wed Sep 16 02:26:06 UTC 2020 lumex-bb- // converse with STM32F407
 
-// Wed Jun 17 08:49:45 UTC 2020 // Okay for Lumex 96x8 display - not great.
-// kidari nefalo
-
-// gelron pibulatto
-
-// STATUS: very good responsiveness, except a
-// possible typing feedback latency (slight if even present).
-
-// Today's changes seem to have helped but were done in
-// such a way that tracking which changes were best, was
-// difficult.
-
-// So, some changes may not be necessary or desirable.
-
-// Still, improved behavior .. getting difficult to tell
-// if further improvement can be appreciated or not.
-
-// 21 May 2020, 22:33:55z
-
-// Mon Dec 23 13:59:52 UTC 2019 // STM32F405 Express running C.H. Ting's eForth
-// Sat Jun  8 00:38:57 UTC 2019
-// fyndov pdxva
-
-// jifx
-
-// #define WEIGHT=1400
-// #define WEIGHT 3400
-// may 21 2221z working with 3400 #define WEIGHT 3400
-// #define WEIGHT 1200
-#define WEIGHT 1800
-// discussion: less than 1200 created input echo blocking stuff.
-// was necessary to space bar flog to get complete feedback,
-// so this WEIGHT must be > 800 or so (probably 1200 or so).
-// NOTE: seems to have an absurd upper bound (tried 9600 and
-// program continued to function about the same).
+// chvargu  kidari  nefalo  gelron  pibulatto
 
 #include <Arduino.h>
-// #include "compatibility.h"
+
+#define LOCAL_ECHO_ON
+#undef LOCAL_ECHO_ON
+
+#define WEIGHT 1800
+#define SERIAL Serial1  // the UART is on Serial1
 
 extern void blink_m(void);
 extern void setup_dotstar(void); // dotstar.cpp
-
-#define SERIAL Serial1  // the UART is on Serial1
 
 const int STKSIZE = 8;
 const int STKMASK = 7;
@@ -87,69 +56,30 @@ void seriAvail() { // may very well be responsible for the eForth bug
       if (SERIAL.available()) {
         printing();
         if (!SERIAL.available()) delay(1);
-        // delay(1); // makes a huge difference to have this tiny delay
-        // break;
     }
   }
 }
 
 void local_echo(void) {
-  // Serial.print("DEBUG aa");
-  // if (ch == '\n') { Serial.print(' '); return; }
-  // WAS ENABLED:
   Serial.print(ch);     // TODO: only if 'echo ON'
-  // Serial.print('\n');
-/*
-  if (ch == '\n')     Serial.print("\r");
-  if (ch == '\r') Serial.print("\n"); // way over
-*/
 }
 
 byte reading() {
   if (!Serial.available()) return 1;
   ch = Serial.read();   // local keystroke is read
-  // delay(3);
+#ifdef LOCAL_ECHO_ON
   local_echo();         // echo that character, locally // KLUDGE
-
-  // translate return to newline only just before sending out the UART:
-  // WAS ENABLED:
-  // if (ch == '\r') ch = '\n'; // Control M becomes Control J - for convenience of human (REMAP KEYSTROKE)
+#endif
 
   SERIAL.write(ch);     // immediately send it out the UART port
 
-  // prod eForth:
-  if (ch == '\t') { // supposed to be '\r' tnr 17 June KludgeWerks
-    // HERE HERE 21 MAY
+  if (ch == '\r') { // supposed to be '\r' tnr 17 June KludgeWerks
   while (!SERIAL.available()); // new blocking 21 may 2207z
-  // discussion - this infinite while didn't break anything obvious.
-  // seems to resolve the issue nicely.  The delay below (24 atm)
-  // may now be superfluous, and other delays are also now suspect.
-  // even 'WEIGHT' may be suspect.
-
-  // This is big news!
-
-    // delay(24); // originally '12' not '1' but '1' seems to work. try 12 if unresponsive.
-
-    // discussion: this 'delay(24)' allows an activity on the STM32F407 to complete.
 
     Serial.print("");
     SERIAL.print(""); // prod eForth maybe
-    // FINDINGS: more is less - do NOT delay or output will be lost.
   }
-  // ^^ eForth doesn't like to report, unprodded,
-  //    on most (but not all) words.
-  //    .S is an exception, for example.
-  //    Had suspected eForth itself was
-  //    coded somewhat unintuitively; now
-  //    it seems more likely that serial
-  //    timing is responsible for the 'bug'
-  //    ('failure to complete without an
-  //    '.. additional space/other char
-  //    '.. sent to eForth, after having
-  //    '.. pressed ENTER').
-
   seriAvail();
-  // seriAvail();
 }
 
 void readword() {
@@ -160,25 +90,16 @@ void readword() {
 /* Arduino main loop */
 
 void setup() {
-  // Serial.begin(38400); // USB
   Serial.begin(115200); // USB
   setup_dotstar(); // turn off dotstar (apa-102 RGB LED)
 
-  /*
-  while (!SERIAL);  // alternate syntax when an empty test is wanted
-  */
   while (!Serial) {
     blink_m();
   }
 
   Serial.println ("terminal - based on the Forth-like interpreter\n");
-  Serial.println ("    16 SEP 2020     cc388e60943bfb9 ");
-  // delay(100);
-  // SERIAL.begin(38400); // TX/RX pair
-  // worked well with eForth // SERIAL.begin(115200); // TX/RX pair
-  // SERIAL.begin(115200); // TX/RX pair
+  Serial.println ("    16 SEP 2020     cc388e60943b-b01 ");
   SERIAL.begin(115200); // TX/RX pair
-  // SERIAL.begin(57600); // assume half clock speed mecrisp // TX/RX pair
 }
 
 void loop() {
