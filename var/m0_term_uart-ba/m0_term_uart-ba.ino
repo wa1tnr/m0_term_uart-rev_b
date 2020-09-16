d0ea2b3f95e6424d89de2e52cd7df54cfe479055
wa1tnr <wa1tnr@users.noreply.github.com>
2020-09-16 01:33:07 +0000

    New Repository - lumex-bb- branch
    Rewrite the terminal to use Serial.peek();
    
    	new file:   m0_term_uart-ba.ino
    
    On branch lumex-bb-
    
    Wed Sep 16 01:32:53 UTC 2020

---
+++ var/m0_term_uart-ba/m0_term_uart-ba.ino
@@ -0,0 +1,19 @@
+// Wed Sep 16 01:25:51 UTC 2020
+
+// redo entire sketch using peeks - 16 September 2020
+
+// old outdated info follows.
+
+
+// Wed May 20 15:57:58 UTC 2020
+// status: works with current hardware setup (ItsyBitsyM4, STM32F407 Discovery)
+
+// Mon Dec 23 19:55:26 UTC 2019
+
+// for STM32F405 project - this sketch runs on Trinket M0,
+// as part of a two-targt project (eForth).
+
+// old:
+// Wed Jan 31 00:39:34 UTC 2018
+// 4737-a0e-03a-
+
